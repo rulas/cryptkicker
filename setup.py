97f316a3291e53c2d266003e544cafa04df9b03b
Raul Villanueva <raul.villanueva@intel.com>
2013-04-11 04:08:25 -0500

    adding a setup.py
    basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cryptkicker',
+    version='0.1',
+    packages=['cryptkicker'],
+    url='',
+    license='',
+    author='Raul Villanueva',
+    author_email='raulvlopez@gmail.com',
+    description='This is a simple tool that performs a basic decryption of a phrase'
+)
