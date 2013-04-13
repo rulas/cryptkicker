e0c03bbb7a3af92cf90a7d06da5a7918598ed8b3
Raul Villanueva <myemail>
2013-04-12 20:18:45 -0500

    minor fixes

--- runcryptkicker.py
+++ runcryptkicker.py
@@ -13,104 +13,103 @@ def decrypt(phrase, seed):
 
 def parse_input(finput):
     """
-    receives a file handler and parses the number of phrases and the prahses to be decrypted
+    receives a file handler and parses the number of phrases and the prahses to be __decrypted
     TODO: add checks for maximum 100 phrases 
     TODO: add checks for maximi 100 chars length for each line
     """
     class States():
         SEARCH_NUM, SEARCH_BLANK, SEARCH_PHRASE, IN_PHRASE, END, ABORTING = range(6)
-    class Events():
-        pass
-        
+
     state = States.SEARCH_NUM
-    num_phrases = 0
+    remaining_phrases = 0
     phrases = list()
     current_phrase = str()
-    
-    for line in finput.readlines():
+    lines = finput.readlines()
+
+    for line in lines:
         
         if state == States.SEARCH_NUM:
-            print "CS: SEARCH_NUM"
+            # print "CS: SEARCH_NUM"
             try:
-                num_phrases = int(line.strip())
-                if num_phrases:
+                remaining_phrases = int(line.strip())
+                if remaining_phrases:
                     state = States.SEARCH_BLANK
-                    print "NS: SEARCH_BLANK"
+                    # print "NS: SEARCH_BLANK"
                 else:
-                    print "Number of phrases is 0"
+                    # print "Number of phrases is 0"
                     state = States.END
-                    print "NS: END"
+                    # print "NS: END"
             except ValueError as e:
-                print "NS: ABORTING"
+                # print "NS: ABORTING"
                 state = States.ABORTING
                 
         elif state == States.SEARCH_BLANK:
-            print "CS: SEARCH_BLANK"      
+            # print "CS: SEARCH_BLANK"
             if line.strip() == '':
                 state = States.SEARCH_PHRASE
-                print "NS: SEARCH_PHRASE"   
+                # print "NS: SEARCH_PHRASE"
             
         elif state == States.SEARCH_PHRASE:
-            print "CS: SEARCH_PHRASE"
+            # print "CS: SEARCH_PHRASE"
             if line.strip() == '':
                 pass
             elif not current_phrase:
                     current_phrase += line.strip()
                     state = States.IN_PHRASE
-                    print "NS: IN_PHRASE"
+                    # print "NS: IN_PHRASE"
                     
         elif state == States.IN_PHRASE:
-            print "CS: SEARCH_PHRASE"
+            # print "CS: SEARCH_PHRASE"
             if line.strip() == '':
                 phrases.append(current_phrase)
                 current_phrase = ''
-                num_phrases -= 1;
-                if not num_phrases:
+                remaining_phrases -= 1
+                state = States.SEARCH_PHRASE
+                if not remaining_phrases:
                     state = States.END
-                    print "NS: END"
+                    # print "NS: END"
             else:
                 current_phrase += " " + line.strip()
-                
+
         elif state == States.END:
-            print "CS: END"
+            # print "CS: END"
             pass
         
         elif state == States.ABORTING:
-            print "CS: ABORTING"
+            # print "CS: ABORTING"
             break
         
     if current_phrase:
         phrases.append(current_phrase)
         
-    return num_phrases, phrases
+    return phrases
 
 
 def main(args):
     #process each of the files submitted as input
     for fpath in args:
-        # process the input fpath for input pharases to be decrypted
+        # process the input fpath for input pharases to be __decrypted
+        phrases = None
         try:
-            print "Processing: %s fpath" % fpath
-            num, phrases = parse_input(open(fpath))
+            print "Processing: %s file" % fpath
+            phrases = parse_input(open(fpath))
         except IOError, e:
             print e
-            
-        if num == 0:
+
+        if not phrases:
             print "No phrases to be decrypted. Quitting"
             sys.exit(-1)
         
         # decrypt each phrase individually
+        seed = u"la zorra cafe rapidamente brinco sobre el perro negro"
         for phrase in phrases:
-            print decrypt(phrase)
+            print decrypt(phrase, seed)
             
             
 
 if __name__ == "__main__":
-    try:
-        main(sys.argv[1:])
-    except:
-        print "There was an problem"
-    
+    main(sys.argv[1:])
+
     
     
 
